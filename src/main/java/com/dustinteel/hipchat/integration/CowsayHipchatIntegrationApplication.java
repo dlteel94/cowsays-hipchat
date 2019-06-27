865f5c3a383a8b98e27c1da4445515ee2ef0f89e
Dustin <dlteel94@gmail.com>
2019-06-27 19:52:40 -0400

    Add base spring boot project

---
+++ src/main/java/com/dustinteel/hipchat/integration/CowsayHipchatIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.dustinteel.hipchat.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CowsayHipchatIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CowsayHipchatIntegrationApplication.class, args);
+	}
+
+}
