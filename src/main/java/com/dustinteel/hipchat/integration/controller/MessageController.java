300755677eaf913842b481cb5d1f0834698e8fba
Dustin <Dustin.Teel@leidos.com>
2019-07-03 10:26:06 -0400

    add request body annotation

--- src/main/java/com/dustinteel/hipchat/integration/controller/MessageController.java
+++ src/main/java/com/dustinteel/hipchat/integration/controller/MessageController.java
@@ -2,6 +2,7 @@ package com.dustinteel.hipchat.integration.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dustinteel.hipchat.integration.model.Incoming;
@@ -9,7 +10,7 @@ import com.dustinteel.hipchat.integration.model.Incoming;
 @RestController
 public class MessageController {
 	@PostMapping("/message-webhook")
-	public HttpStatus processMessage(Incoming incoming) {
+	public HttpStatus processMessage(@RequestBody Incoming incoming) {
 		System.out.println(incoming.getItem().getMessage());
 		return HttpStatus.OK;
 	}
