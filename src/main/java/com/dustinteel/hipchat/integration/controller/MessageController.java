be2a8b8b30daed9e849218c67ff3307dea157f6a
Dustin <Dustin.Teel@leidos.com>
2019-07-03 13:27:01 -0400

    test token

--- src/main/java/com/dustinteel/hipchat/integration/controller/MessageController.java
+++ src/main/java/com/dustinteel/hipchat/integration/controller/MessageController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dustinteel.hipchat.integration.model.Incoming;
@@ -20,8 +21,9 @@ public class MessageController {
 	private HttpServletRequest request;
 	
 	@PostMapping("/message-webhook")
-	public HttpStatus processMessage(@RequestBody Incoming incoming) throws IOException {
+	public HttpStatus processMessage(@RequestBody Incoming incoming, @RequestParam("signed_request") String token) throws IOException {
 		System.out.println(incoming.getItem().getIncomingMessage().getMessage());
+		System.out.println(token);
 		return HttpStatus.OK;
 	}
 }
