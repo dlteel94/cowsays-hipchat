1a544bb03b5b400a9cc98c39e847d45f66f0750f
Dustin <Dustin.Teel@leidos.com>
2019-07-03 10:28:42 -0400

    Add debugging log

--- src/main/java/com/dustinteel/hipchat/integration/controller/MessageController.java
+++ src/main/java/com/dustinteel/hipchat/integration/controller/MessageController.java
@@ -1,5 +1,11 @@
 package com.dustinteel.hipchat.integration.controller;
 
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,9 +15,13 @@ import com.dustinteel.hipchat.integration.model.Incoming;
 
 @RestController
 public class MessageController {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
 	@PostMapping("/message-webhook")
-	public HttpStatus processMessage(@RequestBody Incoming incoming) {
-		System.out.println(incoming.getItem().getMessage());
+	public HttpStatus processMessage(@RequestBody Incoming incoming) throws IOException {
+		System.out.println(request.getReader().lines().collect(Collectors.joining(System.lineSeparator())));
 		return HttpStatus.OK;
 	}
 }
