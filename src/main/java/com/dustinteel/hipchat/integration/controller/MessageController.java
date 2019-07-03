5557e3e3a9307bd765bdd3ce34c36f1dbd3006d2
Dustin <Dustin.Teel@leidos.com>
2019-07-03 13:45:14 -0400

    echo message

--- src/main/java/com/dustinteel/hipchat/integration/controller/MessageController.java
+++ src/main/java/com/dustinteel/hipchat/integration/controller/MessageController.java
@@ -1,29 +1,25 @@
 package com.dustinteel.hipchat.integration.controller;
 
 import java.io.IOException;
-import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dustinteel.hipchat.integration.model.HipchatMessage;
 import com.dustinteel.hipchat.integration.model.Incoming;
 
 @RestController
 public class MessageController {
 	
-	@Autowired
-	private HttpServletRequest request;
-	
 	@PostMapping("/message-webhook")
-	public HttpStatus processMessage(@RequestBody Incoming incoming, @RequestParam("signed_request") String token) throws IOException {
-		System.out.println(incoming.getItem().getIncomingMessage().getMessage());
-		System.out.println(token);
-		return HttpStatus.OK;
+	public HipchatMessage processMessage(@RequestBody Incoming incoming) throws IOException {
+		HipchatMessage message = new HipchatMessage();
+		message.setMessage(incoming.getItem().getIncomingMessage().getMessage());
+		return message;
 	}
 }
---
+++ src/main/java/com/dustinteel/hipchat/integration/model/HipchatMessage.java
@@ -0,0 +1,13 @@
+package com.dustinteel.hipchat.integration.model;
+
+public class HipchatMessage {
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
