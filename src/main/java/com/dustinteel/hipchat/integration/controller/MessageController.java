c8de58040baf0bf8371fd549a084779f19375c7f
Dustin <Dustin.Teel@leidos.com>
2019-07-03 14:56:55 -0400

    actually run through cowsay

--- src/main/java/com/dustinteel/hipchat/integration/controller/MessageController.java
+++ src/main/java/com/dustinteel/hipchat/integration/controller/MessageController.java
@@ -2,27 +2,31 @@ package com.dustinteel.hipchat.integration.controller;
 
 import java.io.IOException;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dustinteel.hipchat.integration.model.HipchatMessage;
 import com.dustinteel.hipchat.integration.model.Incoming;
+import com.dustinteel.hipchat.integration.service.CowsaysService;
 
 @RestController
 public class MessageController {
+	
+	@Autowired
+	CowsaysService cowsaysService;
 	
 	@PostMapping("/message-webhook")
 	public HipchatMessage processMessage(@RequestBody Incoming incoming) throws IOException {
 		String message = incoming.getItem().getIncomingMessage().getMessage();
 		if (message.indexOf("/cowsay") == 0) {
-			message = message.substring(6, message.length());
+			message = message.substring(7, message.length());
 		} else {
 			message = "Cannot do things.";
 		}
 		HipchatMessage hipchatMessage = new HipchatMessage();
-		hipchatMessage.setMessage(message);
+		hipchatMessage.setMessage(String.format("<pre>%s</pre>", cowsaysService.generateCowsay(message)));
 		hipchatMessage.setColor("GREEN");
 		hipchatMessage.setMessage_format("html");
 		hipchatMessage.setNotify(true);
